6bd0a2a81ec2108a746e24bf1baf915102d1d172
ReginaF2012 <skritschy@gmail.com>
2020-08-27 11:26:19 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import BandsContainer from './containers/BandsContainer';
+import BandsInput from './components/BandInput';
 
 class App extends Component {
   render() {
---
+++ src/actions/actions.js
@@ -0,0 +1,5 @@
+const ADD_BAND = "ADD_BAND"
+
+export function addBand(payload){
+    return {type: ADD_BAND, payload}
+}
