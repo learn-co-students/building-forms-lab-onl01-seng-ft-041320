d6af93be3a212137109f56ffe2858e8988226178
Franklyn Rodriguez <franklynr92@gmail.com>
2020-08-29 13:55:13 -0400

    Done.

--- src/containers/BandsContainer.js
+++ src/containers/BandsContainer.js
@@ -6,13 +6,16 @@ class BandsContainer extends Component {
   
   render() {
     console.log("what are props in BandsContainer when rendered", this.props)
-    // debugger;
+    debugger;
     return(
       <div>
         <BandInput addBand={this.props.addBand}/>
          {console.log("i'm in the render", this.props)}
-         {console.log("in the bandcontainer render", this.store)}
-        BandsContainer
+         
+        { this.props.bands.length === 0 ?
+        this.props.bands.map((band, index) => <li key={index}>{band.name}</li>)
+        : <li>nothing yet</li>
+        }
       </div>
     )
   }
